7aa4004da369640d4aceff51306cfada0e66195c
muzaffar <muzaffar>
2023-05-27 16:37:12 +0500

    project

---
+++ main.js
@@ -0,0 +1,41 @@
+var name = prompt('yoshingizni kiriting')
+
+if (name <= 18 ) {
+
+    console.log('yoshsiz. oqishingiz kerak');
+
+}else{
+    console.log("xatolik bor takror urinib korin");
+}
+
+
+
+var title = prompt( 'yoshingizni kiriting')
+if (title <= 50 && title >= 18) {
+
+    console.log("ishlashish kere");
+}else{
+    console.log("xatolik bor takror urinib korin");
+}
+
+
+var text = prompt( 'yoshingizni kiriting')
+if (text <= 59 && text >= 50) {
+
+    console.log("yaqinda pensiyaga chiqasiz");
+}else{
+    console.log("xatolik bor takror urinib korin");
+}
+
+
+var img = prompt( 'yoshingizni kiriting')
+if (img <= 150 && img >= 59) {
+
+    console.log("pensionerga oxshaysiz, xali tirik bolsangiz");
+}
+else{
+    console.log("xatolik bor takror urinib korin");
+}
+
+
+
